88b835db9e38a08c31e2eac5595e4150b55d513c
Senealukard <koc.ezenarslan@protonmail.com>
2025-06-21 16:52:43 +0300

    x

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@
 #include <utility>
 
 #define TX_CE_PIN 27
-#define TX_CSN_PIN 10
+#define TX_CSN_PIN 0
 #define RX_CE_PIN 22
-#define RX_CSN_PIN 0
+#define RX_CSN_PIN 10
 
 static constexpr uint64_t BASE_TX = 0xF0F0F0F0E1LL;
 static constexpr uint64_t BASE_RX = 0xF0F0F0F0D2LL;
